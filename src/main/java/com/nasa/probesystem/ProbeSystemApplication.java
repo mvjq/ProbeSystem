c7cc10c9f2dc0a3e4fdc766023052169d5bafa75
Marcos Vinicius Junqueira <marcos.junqueira@sinch.com>
2021-11-27 14:06:45 -0300

    Init project with gradle configuration files

---
+++ src/main/java/com/nasa/probesystem/ProbeSystemApplication.java
@@ -0,0 +1,12 @@
+package com.nasa.probesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbeSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProbeSystemApplication.class, args);
+  }
+}
---
+++ src/test/java/com/nasa/probesystem/ProbesystemApplicationTests.java
@@ -0,0 +1,11 @@
+package com.nasa.probesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbesystemApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
