fc19397079856d0ad987ddf6a388a46b5d591fec
Marcos Vinicius Junqueira <marcos.junqueira@sinch.com>
2021-11-29 21:46:58 -0300

    added navigation logic (not tested yet and with commmands in portuguese)

--- src/main/java/com/nasa/probesystem/domain/service/NavigationService.java
+++ src/main/java/com/nasa/probesystem/domain/service/NavigationService.java
@@ -3,19 +3,28 @@ package com.nasa.probesystem.domain.service;
 import com.nasa.probesystem.domain.model.Command;
 import com.nasa.probesystem.domain.model.Planet;
 import com.nasa.probesystem.domain.model.Probe;
+import com.nasa.probesystem.repository.PlanetRepository;
+import com.nasa.probesystem.repository.ProbeRepository;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class NavigationService implements Navigation {
   private final NavigationValidationService validationService;
+  private final PlanetRepository planetRepository;
+  private final ProbeRepository probeRepository;
 
-  public NavigationService(NavigationValidationService validationService) {
+  public NavigationService(
+      final NavigationValidationService validationService,
+      final PlanetRepository planetRepository,
+      final ProbeRepository probeRepository) {
     this.validationService = validationService;
+    this.planetRepository = planetRepository;
+    this.probeRepository = probeRepository;
   }
 
   @Override
-  public Planet navigate(Planet planet, Probe probe, List<Command> commands) {
+  public Planet navigate(Planet planet, Probe probe, final List<Command> commands) {
 
     /*
     Vou receber um planeta válido e vou pousar esse probe nele
@@ -24,7 +33,7 @@ public class NavigationService implements Navigation {
     A cada movimento eu checo.
     */
     // TODO: melhorar isso e aplicar isso com operações de stream
-    if (validationService.validatedInteractionProbePlanet(probe, planet)) {
+    if (validationService.validatedProbePositionInPlanet(probe, planet)) {
       /*
       Ok, eu validei que a posição do probe está dentro do planeta
       validei também que eu naõ vou pousar em outro probe que tá no planeta (como eu vou buscar isso no banco?)
@@ -32,6 +41,8 @@ public class NavigationService implements Navigation {
       land(planet, probe);
       applyCommands(planet, probe, commands);
     }
+    
+    // TODO: implement error handling here
 
     return new Planet("mars", 0, 1);
   }
@@ -39,9 +50,10 @@ public class NavigationService implements Navigation {
   @Override
   public Planet land(Planet planet, Probe probe) {
     log.info("Landing the probe {} on planet {}", probe, planet);
-    if (validationService.validatedInteractionProbePlanet(probe, planet)) {
+    if (validationService.validatedProbePositionInPlanet(probe, planet)) {
       return planet;
     }
+    //TODO: implement error handling here
     return null;
   }
 
@@ -53,29 +65,53 @@ public class NavigationService implements Navigation {
     } else if (command.equals(Command.R)) {
       probe.setFaceDirection(probe.getFaceDirection().getRight());
     }
+    // implement erro handling here
     return null;
   }
 
   @Override
   public Probe moveForward(Planet planet, Probe probe) {
-    /*
-    mover é um pouco mais dificil
-    */
-    log.info("Moving forwarding planet:{} probe:{}", planet, probe);
 
+    log.info("Moving forwarding planet:{} probe:{}", planet, probe);
     switch (probe.getFaceDirection()) {
       case N:
+        // (x = x, y + 1)
+        moveXandY(probe, probe.getPositionX(), probe.getPositionY() + 1);
+        break;
       case W:
+        // (x - 1, y = y)
+        moveXandY(probe, probe.getPositionX() - 1, probe.getPositionY());
+        break;
       case S:
+        // (x = x, y - 1)
+        moveXandY(probe, probe.getPositionX(), probe.getPositionY() - 1);
+        break;
       case E:
+        // (x + 1, y = y)
+        moveXandY(probe, probe.getPositionX() + 1, probe.getPositionY());
+        break;
       default:
+          // TODO: implemente erro handle here
         log.info("Something went bad when moving the probe forward");
     }
 
+    // validate position (if its not correct, we thrown an error (InvalidCommandPosition or
+    // something)
+    // if is valid, we save the probe
+    if (validationService.validatedProbePositionInPlanet(probe, planet)) {
+      // save and return the probe
+      log.info(
+          "The probe {} move foward in the planet {} and stayed in a valid position",
+          probe,
+          planet);
+      return null;
+    }
+    // TODO implement erro handling here
+    log.info("Something went wrong with the probe {} moving forward", probe);
     return null;
   }
 
-  private Probe applyCommands(Planet planet, Probe probe, List<Command> commands) {
+  private Probe applyCommands(Planet planet, Probe probe, final List<Command> commands) {
     log.info("applying commands {} planet {} probe {}", commands, planet, probe);
     commands.forEach(
         command -> {
@@ -92,8 +128,11 @@ public class NavigationService implements Navigation {
               log.info("Command now found, throwing InvalidCommandException");
           }
         });
-
-    // probe que agora tá em outro lugar
     return probe;
+  }
+
+  private void moveXandY(Probe probe, int x, int y) {
+    probe.setPositionX(x);
+    probe.setPositionY(y);
   }
 }
