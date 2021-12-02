189bf57edec6195e17826e748f35d9425c68f044
Marcos Vinicius Junqueira <marcos.junqueira@sinch.com>
2021-12-01 23:04:51 -0300

    Added interface to document navigation api controller

--- src/main/java/com/nasa/probesystem/web/controller/v1/ProbeNavigationApi.java
+++ src/main/java/com/nasa/probesystem/web/controller/v1/ProbeNavigationApi.java
@@ -12,105 +12,98 @@ import org.springframework.http.ResponseEntity;
 
 interface ProbeNavigationApi {
 
-    @Operation(
-            summary = "Create new probe navigation",
-            description = "Created a new navigation plan to the probe on a planet using commands.")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(
-                            responseCode = "201",
-                            description = "Object created",
-                            content = {
-                                    @Content(
-                                            mediaType = "application/json",
-                                            schema = @Schema(implementation = ProbeSystemResponse.class))
-                            }),
-                    @ApiResponse(
-                            responseCode = "500",
-                            description = "Something bad happens inside the data access service",
-                            content = @Content(schema = @Schema(hidden = true)))
-            })
+  @Operation(
+      summary = "Create new probe navigation",
+      description = "Created a new navigation plan to the probe on a planet using commands.")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "201",
+            description = "Object created",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ProbeSystemResponse.class))
+            }),
+        @ApiResponse(
+            responseCode = "500",
+            description = "Something bad happens inside the data access service",
+            content = @Content(schema = @Schema(hidden = true)))
+      })
   ResponseEntity<ProbeSystemResponse> createProbeNavigation(ProbeSystemRequest request);
 
-    @Operation(
-            summary = "Get a probe",
-            description = "Get a probe by it id.")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(
-                            responseCode = "302",
-                            description = "Object found",
-                            content = {
-                                    @Content(
-                                            mediaType = "application/json",
-                                            schema = @Schema(implementation = ProbeSystemResponse.class))
-                            }),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Object not found",
-                            content = @Content(schema = @Schema(hidden = true)))
-            })
+  @Operation(summary = "Get a probe", description = "Get a probe by it id.")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "302",
+            description = "Object found",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ProbeSystemResponse.class))
+            }),
+        @ApiResponse(
+            responseCode = "404",
+            description = "Object not found",
+            content = @Content(schema = @Schema(hidden = true)))
+      })
   ResponseEntity<ProbeSystemResponse> getProbe(int probeId);
 
-    @Operation(
-            summary = "Get all probe",
-            description = "Get all probes in the database.")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(
-                            responseCode = "302",
-                            description = "Object found",
-                            content = {
-                                    @Content(
-                                            mediaType = "application/json",
-                                            schema = @Schema(implementation = ProbeSystemResponse.class))
-                            }),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Object not found",
-                            content = @Content(schema = @Schema(hidden = true)))
-            })
-    ResponseEntity<List<ProbeSystemResponse>> getAllProbes();
+  @Operation(summary = "Get all probe", description = "Get all probes in the database.")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "302",
+            description = "Object found",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ProbeSystemResponse.class))
+            }),
+        @ApiResponse(
+            responseCode = "404",
+            description = "Object not found",
+            content = @Content(schema = @Schema(hidden = true)))
+      })
+  ResponseEntity<List<ProbeSystemResponse>> getAllProbes();
 
-    @Operation(
-            summary = "Updated a probe navigation system.",
-            description = "Updated a probe navigation system with new commands")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(
-                            responseCode = "200",
-                            description = "OK",
-                            content = {
-                                    @Content(
-                                            mediaType = "application/json",
-                                            schema = @Schema(implementation = ProbeSystemResponse.class))
-                            }),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Object not found",
-                            content = @Content(schema = @Schema(hidden = true)))
-            })
+  @Operation(
+      summary = "Updated a probe navigation system.",
+      description = "Updated a probe navigation system with new commands")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "OK",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ProbeSystemResponse.class))
+            }),
+        @ApiResponse(
+            responseCode = "404",
+            description = "Object not found",
+            content = @Content(schema = @Schema(hidden = true)))
+      })
   ResponseEntity<ProbeSystemResponse> updatedProbeNavigation(
       int probeId, ProbeSystemRequest request);
 
-
-    @Operation(
-            summary = "Delete a probe",
-            description = "Delete a probe by its id.")
-    @ApiResponses(
-            value = {
-                    @ApiResponse(
-                            responseCode = "302",
-                            description = "Object found",
-                            content = {
-                                    @Content(
-                                            mediaType = "application/json",
-                                            schema = @Schema(implementation = ProbeSystemResponse.class))
-                            }),
-                    @ApiResponse(
-                            responseCode = "404",
-                            description = "Object not found",
-                            content = @Content(schema = @Schema(hidden = true)))
-            })
+  @Operation(summary = "Delete a probe", description = "Delete a probe by its id.")
+  @ApiResponses(
+      value = {
+        @ApiResponse(
+            responseCode = "302",
+            description = "Object found",
+            content = {
+              @Content(
+                  mediaType = "application/json",
+                  schema = @Schema(implementation = ProbeSystemResponse.class))
+            }),
+        @ApiResponse(
+            responseCode = "404",
+            description = "Object not found",
+            content = @Content(schema = @Schema(hidden = true)))
+      })
   ResponseEntity<ProbeSystemResponse> deleteProbe(int probeId);
 }
