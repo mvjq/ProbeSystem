e07a41e838baf8a6584d007801bfc0ed97b948e4
Marcos Vinicius Junqueira <marcos.junqueira@sinch.com>
2021-12-02 22:36:27 -0300

    Added service NavigationValidationService which checks if the interaction between probe, planet and other probes are correct. Tests included!.

--- src/main/java/com/nasa/probesystem/domain/service/NavigationValidationService.java
+++ src/main/java/com/nasa/probesystem/domain/service/NavigationValidationService.java
@@ -1,9 +1,91 @@
 package com.nasa.probesystem.domain.service;
 
-public class NavigationValidationService {
-  /*
-  this module contains all the validation of the state of the objects
-  because the domain logic is similar to the real world, is easier
-  to crate rules and apply then to an state to check if is stays correct
-   */
+import com.nasa.probesystem.domain.model.Planet;
+import com.nasa.probesystem.domain.model.Probe;
+import com.nasa.probesystem.repository.PlanetRepository;
+import com.nasa.probesystem.repository.ProbeRepository;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class NavigationValidationService implements NavigationValidation {
+
+  private final PlanetRepository planetRepository;
+
+  public NavigationValidationService(
+      PlanetRepository planetRepository, ProbeRepository probeRepository) {
+    this.planetRepository = planetRepository;
+    this.probeRepository = probeRepository;
+  }
+
+  @Override
+  public Boolean validateProbe(Probe probe) {
+    log.info("probe info: {}", probe);
+    if (probe.getFaceDirection() == null || probe.getPlanet() == null) {
+      log.info("Probe {} is not valid", probe);
+      return Boolean.FALSE;
+    }
+    return Boolean.TRUE;
+  }
+
+  @Override
+  public Boolean validatePlanet(Planet planet) {
+
+    var found = planetRepository.findById(planet.getId());
+    if (found.isEmpty()
+        || planet.getPlanetName() == null
+        || planet.getMaxX() < 0
+        || planet.getMaxY() < 0) {
+
+      log.info("Planet {} is not valid", planet);
+      return Boolean.FALSE;
+    }
+    log.info("Planet found in the database and its valid: {}");
+    return Boolean.TRUE;
+  }
+
+  @Override
+  public Boolean validateProbePositionInPlanet(Probe probe, Planet planet) {
+    var validPlanet = planetRepository.findById(planet.getId()).orElse(null);
+    if (validPlanet == null) return Boolean.FALSE;
+
+    var listProbes = planetRepository.findAllProbesByplanetId(validPlanet.getId());
+    if (!validateCollisionProbes(probe, listProbes) || !validateXandYOnPlanet(probe, planet)) {
+      log.info("The probe {} cant move or land in the planet {}", probe, planet);
+      return Boolean.FALSE;
+    }
+    return Boolean.TRUE;
+  }
+
+  private Boolean validateXandYOnPlanet(Probe probe, Planet planet) {
+    if (probe.getPositionX() > planet.getMaxX()
+        || probe.getPositionY() > planet.getMaxY()
+        || -probe.getPositionX() < -planet.getMaxX()
+        || -probe.getPositionY() < -planet.getMaxY()) {
+      log.info("The probe {} coordinates are not valid in the planet {}", probe, planet);
+      return Boolean.FALSE;
+    }
+    return Boolean.TRUE;
+  }
+
+  private Boolean validateCollisionProbes(Probe probe, List<Probe> listProbes) {
+    for (Probe probeOnPlanet : listProbes) {
+      if (!validateProbeCollision(probe, probeOnPlanet)) {
+        log.info("The new probe {} entered a colision with probe {} probeOnPlanet");
+        return Boolean.FALSE;
+      }
+    }
+    return Boolean.TRUE;
+  }
+
+  private Boolean validateProbeCollision(Probe probe, Probe probeOnPlanet) {
+    if (probe.getPositionX() == probeOnPlanet.getPositionX()
+        || probe.getPositionY() == probeOnPlanet.getPositionY()) {
+      log.info("A collision between probes happened");
+      return Boolean.FALSE;
+    }
+    return Boolean.TRUE;
+  }
 }
---
+++ src/test/java/com/nasa/probesystem/domain/service/NavigationValidationTests.java
@@ -0,0 +1,122 @@
+package com.nasa.probesystem.domain.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.nasa.probesystem.domain.model.Direction;
+import com.nasa.probesystem.domain.model.Planet;
+import com.nasa.probesystem.domain.model.Probe;
+import com.nasa.probesystem.repository.PlanetRepository;
+import com.nasa.probesystem.repository.ProbeRepository;
+import java.util.List;
+import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@Slf4j
+@ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class NavigationValidationTests {
+
+  private PlanetRepository planetRepositoryMock = mock(PlanetRepository.class);
+  private ProbeRepository probeRepositoryMock = mock(ProbeRepository.class);
+  private NavigationValidationService underTest =
+      new NavigationValidationService(planetRepositoryMock, probeRepositoryMock);
+
+  @Test
+  void validPlanet_shouldReturnTrue() {
+    Planet planet = givenGetValidPlanet("Mars", 5, 5, 1);
+    assertTrue(underTest.validatePlanet(planet));
+  }
+
+  @Test
+  void emptyPlanet_shouldReturnFalse() {
+    Planet emptyPlanet = Planet.builder().build();
+    assertEquals(Boolean.FALSE, underTest.validatePlanet(emptyPlanet));
+  }
+
+  @Test
+  void validProbe_shouldReturnTrue() {
+    Planet planet = givenGetValidPlanet("Mars", 5, 5, 1);
+    Probe probe = givenGetValidProbe("mars rover", 0, 0, Direction.N, planet);
+    var flag = underTest.validateProbe(probe);
+    assertTrue(flag);
+  }
+
+  @Test
+  void emptyProbe_shouldReturnFalse() {
+    Probe emptyProbe = Probe.builder().build();
+    assertEquals(Boolean.FALSE, underTest.validateProbe(emptyProbe));
+  }
+
+  @Test
+  void validProbePositionInPlanet_shouldReturnTrue() throws Exception {
+    Planet planet = givenGetValidPlanet("Mars", 5, 5, 1);
+    Probe probe = givenGetValidProbe("mars roover", 0, 0, Direction.N, planet);
+    assertTrue(underTest.validateProbePositionInPlanet(probe, planet));
+  }
+
+  @Test
+  void invalidProbePositionInPlanet_ProbeIsOutsideOfPlanetLimits_shouldReturnFalse() {
+    Planet planet = givenGetValidPlanet("Mars", 5, 5, 1);
+    Probe probe = givenGetValidProbe("mars roover", 10, 10, Direction.N, planet);
+    assertFalse(underTest.validateProbePositionInPlanet(probe, planet));
+  }
+
+  @Test
+  void invalidProbePositionInPlanet_CollisionWithProbes_shouldReturnFalse_1() {
+    Planet planet = givenGetValidPlanet("Mars", 10, 10, 1);
+    injectMultipleProbesInRepository(planet);
+    Probe probe = givenGetValidProbe("mars roover #5", 0, 0, Direction.E, planet);
+    assertFalse(underTest.validateProbePositionInPlanet(probe, planet));
+  }
+
+  @Test
+  void invalidProbePositionInPlanet_CollisionWithProbes_shouldReturnFalse_2() {
+    Planet planet = givenGetValidPlanet("Mars", 10, 10, 1);
+    injectMultipleProbesInRepository(planet);
+    Probe probe = givenGetValidProbe("mars roover #5", 1, 2, Direction.E, planet);
+    assertFalse(underTest.validateProbePositionInPlanet(probe, planet));
+  }
+
+  @Test
+  void invalidProbePositionInPlanet_CollisionWithProbes_shouldReturnFalse_3() {
+    Planet planet = givenGetValidPlanet("Mars", 10, 10, 1);
+    injectMultipleProbesInRepository(planet);
+    Probe probe = givenGetValidProbe("mars roover #5", 3, 2, Direction.E, planet);
+    assertFalse(underTest.validateProbePositionInPlanet(probe, planet));
+  }
+
+  private Planet givenGetValidPlanet(String name, int x, int y, int id) {
+    var planet = Planet.builder().planetName(name).maxX(x).maxY(y).id(id).build();
+    when(planetRepositoryMock.findById(planet.getId())).thenReturn(Optional.of(planet));
+    return planet;
+  }
+
+  private Probe givenGetValidProbe(
+      String name, int x, int y, Direction faceDirection, Planet planet) {
+    return Probe.builder()
+        .probeName(name)
+        .positionX(x)
+        .positionY(y)
+        .id(1)
+        .planet(planet)
+        .faceDirection(faceDirection)
+        .build();
+  }
+
+  private void injectMultipleProbesInRepository(Planet planet) {
+    var listProbes =
+        List.of(
+            givenGetValidProbe("mars roover #1", 0, 0, Direction.N, planet),
+            givenGetValidProbe("mars roover #2", 1, 2, Direction.N, planet),
+            givenGetValidProbe("mars roover #3", 2, 2, Direction.N, planet));
+    when(planetRepositoryMock.findAllProbesByplanetId(planet.getId())).thenReturn((listProbes));
+  }
+}
