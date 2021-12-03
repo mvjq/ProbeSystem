d0362aeb45fa42be0cc0668c0e54f9f8a8c4a9c5
Marcos Vinicius Junqueira <marcos.junqueira@sinch.com>
2021-12-03 04:47:23 -0300

    Renaming service test class

--- src/test/java/com/nasa/probesystem/domain/service/ProbeNavigationServiceTests.java
+++ src/test/java/com/nasa/probesystem/domain/service/ProbeNavigationServiceTests.java
@@ -16,7 +16,7 @@ import java.util.List;
 import javax.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 
-class ProbeNavigationSystemTests {
+class ProbeNavigationServiceTests {
   private final ProbeRepository probeRepositoryMock = mock(ProbeRepository.class);
   private final PlanetRepository planetRepositoryMock = mock(PlanetRepository.class);
   private final DataAccessService dataAccessService =
@@ -25,8 +25,8 @@ class ProbeNavigationSystemTests {
       new NavigationValidationService(dataAccessService);
   private final NavigationService navigationServiceMock =
       new NavigationService(navigationValidationMock);
-  private final ProbeNavigationSystem underTest =
-      new ProbeNavigationSystem(navigationServiceMock, dataAccessService);
+  private final ProbeNavigationService underTest =
+      new ProbeNavigationService(navigationServiceMock, dataAccessService);
 
   @Test
   void createProbeNavigation_validPlanetValidProbe_shouldSucceed() throws Exception {
@@ -34,16 +34,16 @@ class ProbeNavigationSystemTests {
     Probe probe = TestUtils.givenGetValidProbe("mars roove", 5, 5, Direction.N, planet);
     List<Command> listOfCommands = List.of(Command.L, Command.R, Command.M, Command.M);
 
-      var landedResponse =
-              underTest.createProbeNavigation(
-                      ProbeSystemRequest.builder()
-                              .planet(planet)
-                              .probe(probe)
-                              .commands(listOfCommands)
-                              .build());
-      System.out.println(landedResponse);
-      assertEquals(landedResponse.getPlanet(), planet);
-      assertEquals(landedResponse.getProbe(), probe);
+    var landedResponse =
+        underTest.createProbeNavigation(
+            ProbeSystemRequest.builder()
+                .planet(planet)
+                .probe(probe)
+                .commands(listOfCommands)
+                .build());
+    System.out.println(landedResponse);
+    assertEquals(landedResponse.getPlanet(), planet);
+    assertEquals(landedResponse.getProbe(), probe);
   }
 
   @Test
@@ -82,38 +82,39 @@ class ProbeNavigationSystemTests {
 
   @Test
   void createProbeNavigation_withCollision_shouldFail() throws Exception {
-      Planet planet = TestUtils.givenGetValidPlanet("mars", 10, 10, 1, planetRepositoryMock);
-      Probe probe = TestUtils.givenGetValidProbe("mars roove", 0, 0, Direction.N, planet);
-      TestUtils.injectMultipleProbesInRepository(planet, planetRepositoryMock);
-      List<Command> listOfCommands = List.of(Command.L, Command.R, Command.M, Command.M);
+    Planet planet = TestUtils.givenGetValidPlanet("mars", 10, 10, 1, planetRepositoryMock);
+    Probe probe = TestUtils.givenGetValidProbe("mars roove", 0, 0, Direction.N, planet);
+    TestUtils.injectMultipleProbesInRepository(planet, planetRepositoryMock);
+    List<Command> listOfCommands = List.of(Command.L, Command.R, Command.M, Command.M);
 
-      assertThrows(
-              Exception.class,
-              () -> underTest.createProbeNavigation(
-                      ProbeSystemRequest.builder()
-                              .planet(planet)
-                              .probe(probe)
-                              .commands(listOfCommands)
-                              .build()));
+    assertThrows(
+        Exception.class,
+        () ->
+            underTest.createProbeNavigation(
+                ProbeSystemRequest.builder()
+                    .planet(planet)
+                    .probe(probe)
+                    .commands(listOfCommands)
+                    .build()));
   }
 
   @Test
   void createProbeNavigation_moveOutOfBounds_shouldMoveToPlanetLimit() throws Exception {
-      Planet planet = TestUtils.givenGetValidPlanet("mars", 4, 4, 1, planetRepositoryMock);
-      Probe probe = TestUtils.givenGetValidProbe("mars roove", 0, 0, Direction.N, planet);
-      List<Command> listOfCommands = List.of(Command.M, Command.M, Command.M, Command.M, Command.M, Command.M);
+    Planet planet = TestUtils.givenGetValidPlanet("mars", 4, 4, 1, planetRepositoryMock);
+    Probe probe = TestUtils.givenGetValidProbe("mars roove", 0, 0, Direction.N, planet);
+    List<Command> listOfCommands =
+        List.of(Command.M, Command.M, Command.M, Command.M, Command.M, Command.M);
 
-
-      var landedResponse =
-              underTest.createProbeNavigation(
-                      ProbeSystemRequest.builder()
-                              .planet(planet)
-                              .probe(probe)
-                              .commands(listOfCommands)
-                              .build());
-      System.out.println(landedResponse);
-      assertEquals(landedResponse.getPlanet(), planet);
-      assertEquals(landedResponse.getProbe(), probe);
-      assertEquals(landedResponse.getProbe().getPositionY(), landedResponse.getPlanet().getMaxY());
+    var landedResponse =
+        underTest.createProbeNavigation(
+            ProbeSystemRequest.builder()
+                .planet(planet)
+                .probe(probe)
+                .commands(listOfCommands)
+                .build());
+    System.out.println(landedResponse);
+    assertEquals(landedResponse.getPlanet(), planet);
+    assertEquals(landedResponse.getProbe(), probe);
+    assertEquals(landedResponse.getProbe().getPositionY(), landedResponse.getPlanet().getMaxY());
   }
 }
